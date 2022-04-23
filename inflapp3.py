84758c8842a76508aa43af50cfd57bb7f4ce4323
arijit sarkar <91779253+mercuryandmercury@users.noreply.github.com>
2022-04-23 13:18:30 +0530

    Update inflapp3.py

--- inflapp3.py
+++ inflapp3.py
@@ -14,13 +14,13 @@ st.line_chart(final)
 st.area_chart(final)
 
 num = st.number_input('Insert the desired number of months',value=0,step=1)
-
+n=num
 X= final.values
 X= X.astype('float32')
 model= ARIMA(X,order = (2,0,1))
 model_fit= model.fit()
-forecasted= model_fit.forecast(steps = num)
-future_year= [final.index[-1]+i+1 for i in range(0,48-n)]
+forecasted= model_fit.forecast(steps = n)
+future_year= [final.index[-1]+i+1 for i in range(0,n)]
 future_df= pd.DataFrame(index = future_year,columns = final.columns )
 future_df['cpi']= forecasted
 
