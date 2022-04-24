ea744edb4be6320ccf5e5c56641ef3c1c14943ac
arijit sarkar <91779253+mercuryandmercury@users.noreply.github.com>
2022-04-24 17:07:39 +0530

    Add files via upload

---
+++ infl5years.py
@@ -0,0 +1,117 @@
+import pandas as pd
+import pickle
+import pandas as pd
+import streamlit as st
+import klib
+from statsmodels.tsa.arima.model import AR
+
+
+model=pickle.load(open('infl2.pkl','rb'))
+
+print(model)
+
+data = pd.read_csv('indianinflationdata.csv')
+df1=data.T
+df1.columns =["cpi"]
+df1.index = pd.to_datetime(df1.index)
+df1.index.names = ['year']
+upsampled = df1.resample('MS')
+final = upsampled.interpolate(method='linear')
+print(final)
+st.line_chart(final)
+st.area_chart(final)
+
+st.line_chart(model)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
